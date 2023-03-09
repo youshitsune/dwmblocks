c53a8a6708530c2175ecd895e615c3735f5fa6eb
youshitsune <luka@youshitsune.me>
2023-03-09 14:33:58 +0100

    Add another pipe for better look

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "/home/luka/.config/dwmblocks/music.sh", 0, 10},
+	{"|", "/home/luka/.config/dwmblocks/music.sh", 0, 10},
 	{"", "/home/luka/.config/dwmblocks/brightness.sh", 0, 10},
 	{"", "/home/luka/.config/dwmblocks/time.sh",60,0},
 };
